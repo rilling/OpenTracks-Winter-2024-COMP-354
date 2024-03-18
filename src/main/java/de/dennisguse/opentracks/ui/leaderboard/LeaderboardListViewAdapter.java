acea573c46d84ed6066beab605ab10762f370471
Palmarino DiMarco <palmardimarco@icloud.com>
2024-03-18 01:29:47 -0400

    Added some boilerplate files to start working on the leaderboard.

---
+++ src/main/java/de/dennisguse/opentracks/ui/leaderboard/LeaderboardListViewAdapter.java
@@ -0,0 +1,62 @@
+package de.dennisguse.opentracks.ui.leaderboard;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import de.dennisguse.opentracks.ui.leaderboard.placeholder.PlaceholderContent.PlaceholderItem;
+import de.dennisguse.opentracks.databinding.FragmentLeaderboardListBinding;
+
+import java.util.List;
+
+/**
+ * {@link RecyclerView.Adapter} that can display a {@link PlaceholderItem}.
+ * TODO: Replace the implementation with code for your data type.
+ */
+public class LeaderboardListViewAdapter extends RecyclerView.Adapter<LeaderboardListViewAdapter.ViewHolder> {
+
+    private final List<PlaceholderItem> mValues;
+
+    public LeaderboardListViewAdapter(List<PlaceholderItem> items) {
+        mValues = items;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        return new ViewHolder(FragmentLeaderboardListBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false));
+
+    }
+
+    @Override
+    public void onBindViewHolder(final ViewHolder holder, int position) {
+        holder.mItem = mValues.get(position);
+        holder.mIdView.setText(mValues.get(position).id);
+        holder.mContentView.setText(mValues.get(position).content);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mValues.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public final TextView mIdView;
+        public final TextView mContentView;
+        public PlaceholderItem mItem;
+
+        public ViewHolder(FragmentLeaderboardListBinding binding) {
+            super(binding.getRoot());
+            mIdView = binding.itemNumber;
+            mContentView = binding.content;
+        }
+
+        @Override
+        public String toString() {
+            return super.toString() + " '" + mContentView.getText() + "'";
+        }
+    }
+}
