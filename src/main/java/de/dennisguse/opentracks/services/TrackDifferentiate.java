e0d4ea074d343ae695e92d85b6020944a8846e0a
JayHyeokLee <123212773+JayHyeokLee@users.noreply.github.com>
2024-03-19 21:24:10 -0400

    Update TrackDifferentiate.java

--- src/main/java/de/dennisguse/opentracks/services/TrackDifferentiate.java
+++ src/main/java/de/dennisguse/opentracks/services/TrackDifferentiate.java
@@ -1,24 +1,37 @@
 package de.dennisguse.opentracks.services;
+
 import de.dennisguse.opentracks.data.models;
 
 public class TrackDifferentiate {
 
     private final ContentProviderUtils contentProviderUtils;
     private final Track.Id trackId;
+    private int liftCount, runCount;
 
-    public TrackDifferentiate(){
-        //list of tracks that contains all the trackpoints of each run
-        ArrayList<ArrayList <SkiLiftPoint>> lifts = new ArrayList<ArrayList <SkiLiftPoint>>();
-        ArrayList<ArrayList <SkiRunPoint>> runs = new ArrayList<ArrayList <SkiRunPoint>>();
+    public TrackDifferentiate() {
+        // list of tracks that contains all the trackpoints of each run
+        ArrayList<ArrayList<SkiLiftPoint>> lifts = new ArrayList<ArrayList<SkiLiftPoint>>();
+        ArrayList<ArrayList<SkiRunPoint>> runs = new ArrayList<ArrayList<SkiRunPoint>>();
 
-        //list of trackpoints grouped per run
+        // list of trackpoints grouped per run
         ArrayList<SkiLiftPoint> liftPoints = new ArrayList<SkiLiftPoint>();
         ArrayList<SkiLiftPoint> runPoints = new ArrayList<SkiRunPoint>();
+
+        liftCount = 0;
+        runCount = 0;
     }
 
     private void differentiate(Track.Id currentTrack){
         try (TrackPointIterator tpi = contentProviderUtils.getTrackPointLocationIterator(currentTrack, null)) {
+            while(tpi.hasNext()){
+                TrackPoint trackpoint = tpi.next();
+                TrackPoint.Type
+                if(trackpoint.getType() == Type.IDLE){
+                    //do nothing
+                }else if(trackpoint.getType() == Type.IDLE){
 
+                }
+            }
         }
     }
 }
