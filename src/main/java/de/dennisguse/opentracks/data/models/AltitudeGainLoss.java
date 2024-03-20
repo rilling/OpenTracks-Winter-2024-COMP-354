d2c7f44734a0d1705d1cf8e5d7298e064c150053
anthony8393 <anthonytang475@gmail.com>
2024-03-20 13:22:30 -0400

    To try and use altitude to check whenever it is skiing or chairlift

--- src/main/java/de/dennisguse/opentracks/data/models/AltitudeGainLoss.java
+++ src/main/java/de/dennisguse/opentracks/data/models/AltitudeGainLoss.java
@@ -1,30 +1,51 @@
 package de.dennisguse.opentracks.data.models;
 
-public record AltitudeGainLoss(float gain_m, float loss_m) {
-    //to differentiate between chairlift and skiing.
-    private static boolean isSkiing;
-    private static boolean isUsingChairLift;
-    private static boolean isWaiting;
+import java.util.List;
 
-    public AltitudeGainLoss(float gain_m, float loss_m){
+public record AltitudeGainLoss(float gain_m, float loss_m) {
+    private static final double altitudeChangeThreshold = 0;
+
+    private static boolean isSkiing;
+    private static boolean isChairlift;
+
+    public AltitudeGainLoss(float gain_m, float loss_m) {
         this.gain_m = gain_m;
         this.loss_m = loss_m;
-        this.isSkiing = loss_m > 0;
-        this.isUsingChairLift = gain_m < 0;
-        this.isWaiting = isWaiting;
     }
 
-    public boolean isSkiing() {
+    public static boolean isSkiing(){
         return isSkiing;
     }
+    public static boolean isIsChairlift(){
+        return isChairlift;
+    }
+    public boolean shouldStartNewSegment(List<TrackPoint> trackPoints, int currentIndex){
+        TrackPoint currentPoint = trackPoints.get(currentIndex);
+        TrackPoint previousPoint = trackPoints.get(currentIndex - 1);
 
-    public boolean isUsingChairLift() {
-        return isUsingChairLift;
+        Altitude currentAltitude = currentPoint.getAltitude();
+        Altitude previousAltitude = previousPoint.getAltitude();
+
+        if (currentAltitude != null && previousAltitude != null){
+            double altitudeChange = currentAltitude.toM() - previousAltitude.toM();
+            if (altitudeChange > altitudeChangeThreshold){
+                isChairlift = true;
+                isSkiing = false;
+                return true;
+            }
+            else if (altitudeChange < -altitudeChangeThreshold){
+                isChairlift = false;
+                isSkiing = true;
+                return true;
+
+            }
+        }
+        isChairlift = false;
+        isSkiing = false;
+        return false;
     }
 
-    public boolean isWaiting() {
-        return isWaiting;
-    }
+
 
 
 
