323111626f39062b37b8b41fdd29d69e68d2b1cc
shimdevkun <shimdevkun@gmail.com>
2024-03-21 19:28:37 -0400

    #44 - Implement base architecture for visualizing daily statistics

---
+++ src/main/java/de/dennisguse/opentracks/ui/aggregatedStatistics/dailyStats/DailyPlottingModule.java
@@ -0,0 +1,8 @@
+package de.dennisguse.opentracks.ui.aggregatedStatistics.dailyStats;
+
+/**
+ * A module to aid in building the graph for visualizing daily statistics.
+ */
+public class DailyPlottingModule {
+
+}
---
+++ src/main/java/de/dennisguse/opentracks/ui/aggregatedStatistics/dailyStats/DailyRunsProvider.java
@@ -0,0 +1,8 @@
+package de.dennisguse.opentracks.ui.aggregatedStatistics.dailyStats;
+
+/**
+ * A mock service to provide dummy data to test the visualize daily statistics feature.
+ */
+public class DailyRunsProvider {
+    
+}
