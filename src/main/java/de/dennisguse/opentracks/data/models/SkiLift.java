7cac0f120a462ebbc3f4189c0456fe7cd5b7aa4a
RamosEsteban <devestebanramos@gmail.com>
2024-03-22 10:37:51 -0400

    Daily switch added in Aggregated stats page Aggregated daily stats list item page created First iteration of the SkiLift and SkiRun classes

---
+++ src/main/java/de/dennisguse/opentracks/data/models/SkiLift.java
@@ -0,0 +1,22 @@
+package de.dennisguse.opentracks.data.models;
+
+public class SkiLift {
+
+    private String name;
+
+    private Double duration;
+
+    private Double waitingTime;
+
+    private Double averageSlope;
+
+    private Double altitude;
+
+    private Double speed;
+
+    private Double longitude;
+
+    private Double latitude;
+
+    //TODO: Add attributes getters, setters and constructor
+}
