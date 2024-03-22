5040765916d83f0760c64d4ec81181909124bde0
Palmarino DiMarco <palmardimarco@icloud.com>
2024-03-22 19:10:23 -0400

    Removed the fab from the LeaderboardActivity

--- src/main/java/de/dennisguse/opentracks/ui/leaderboard/LeaderboardActivity.java
+++ src/main/java/de/dennisguse/opentracks/ui/leaderboard/LeaderboardActivity.java
@@ -32,14 +32,5 @@ public class LeaderboardActivity extends AppCompatActivity {
         viewPager.setAdapter(sectionsPagerAdapter);
         TabLayout tabs = binding.tabs;
         tabs.setupWithViewPager(viewPager);
-        FloatingActionButton fab = binding.fab;
-
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 }
