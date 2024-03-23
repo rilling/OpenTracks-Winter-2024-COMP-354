ee46dd06ed6fd7e9d794a9aa113c6ab59188ea9d
Tanu Patel <tanpatel25@gmail.com>
2024-03-23 19:50:08 -0400

    temp stats page

---
+++ src/main/java/de/dennisguse/opentracks/Stats.java
@@ -0,0 +1,14 @@
+package de.dennisguse.opentracks;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Stats extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.stats); // This line links stats.xml with this Activity.
+
+    }
+}
