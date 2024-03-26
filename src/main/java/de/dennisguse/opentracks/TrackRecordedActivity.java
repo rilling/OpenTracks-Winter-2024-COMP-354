87f02ca190bd7365f88916cd8211e1a659cba6ed
Kevin Tran <kevinqt@kevins-air.home>
2024-03-25 22:43:30 -0400

    save migrations

--- src/main/java/de/dennisguse/opentracks/TrackRecordedActivity.java
+++ src/main/java/de/dennisguse/opentracks/TrackRecordedActivity.java
@@ -111,7 +111,6 @@ public class TrackRecordedActivity extends AbstractTrackDeleteActivity implement
     @Override
     protected void onStart() {
         super.onStart();
-
         trackDataHub.start();
     }
 
